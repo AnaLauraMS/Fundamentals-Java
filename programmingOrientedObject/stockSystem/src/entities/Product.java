152c7e33f855fdaa8a83cc11092c227a4dcf7daa
AnaLauraMartinsS <ana.laura.martins.souto@gmail.com>
2026-08-21 09:43:03 -0300

    feat: criando um projeto de estoque utilizando os conceitos de POO

---
+++ programmingOrientedObject/stockSystem/src/entities/Product.java
@@ -0,0 +1,24 @@
+package entities;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock(){
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity){
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity){
+        this.quantity -= quantity;
+    }
+}
